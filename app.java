2b2c4666db6e8eeed93a660a3bfb66c693a65d96
Marcyline Guthrie <desertfruit.max@gmail.com>
2025-08-08 17:46:00 -0700

    Changing the calculator to have GUI interface

--- app.java
+++ app.java
@@ -1,5 +1,4 @@
 package calculator;
-import java.util.Scanner;
 /*
  * Author: Marcy Guthrie
  * Purpose: Run a basic calculator that can parse string inputs into results
@@ -8,21 +7,8 @@ import java.util.Scanner;
 
 public class app{
     public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
-        String input;
-        String result;
-        
-        
+
         Calculator calc = new Calculator();
 
-        System.out.println("input: 'number 1' 'opperator' 'number 2'");
-        input = scan.nextLine();
-
-        result = calc.calculate(input);
-
-        System.out.println(result);
-
-
-        scan.close();
     }
 }
--- calculator.java
+++ calculator.java
@@ -5,12 +5,96 @@ package calculator;
  * 
  */
 
-public class Calculator {
-    //No special set up
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Calculator extends JFrame implements ActionListener{
+
+    // --- public variables ---
+    JButton[] numButtons = new JButton[9]; 
+    JButton[] funcButtons = new JButton[4];
+    JButton[] zeButtons = new JButton[2];
+    JTextField textField;
+    JPanel numPanel, textPanel, funcPanel, zePanel;
+    String txt = "waow";
+
     public Calculator(){
 
+        // --- initializing frame --- 
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
+        this.setTitle("Calculator");
+
+        // --- numbers 1-9 panel ---
+        numPanel = new JPanel(new GridLayout(3, 3));
+        for(int ix = 0; ix < 9; ix++){
+            numButtons[ix] = new JButton(Integer.toString(ix+1));
+            numPanel.add(numButtons[ix]);
+            numButtons[ix].addActionListener(this);
+        }
+        numPanel.setBounds(0,50, 150,150);
+
+        // --- function panel ---
+        funcPanel = new JPanel(new GridLayout(4,1));
+        funcButtons[0] = new JButton("+");
+        funcButtons[1] = new JButton("-");
+        funcButtons[2] = new JButton("*");
+        funcButtons[3] = new JButton("/");
+        for(int ix = 0; ix < 4; ix++){
+            funcPanel.add(funcButtons[ix]);
+            funcButtons[ix].addActionListener(this);
+        }
+        funcPanel.setBounds(150,50,50,200);
+
+        // --- zero/enter panel ---
+        zePanel = new JPanel(new GridLayout(1,2));
+        zeButtons[0] = new JButton("0");
+        zeButtons[1] = new JButton("=");
+        for(int ix = 0; ix < 2; ix++){
+            zePanel.add(zeButtons[ix]);
+            zeButtons[ix].addActionListener(this);
+        }
+        zePanel.setBounds(0,200,150,50);
+
+        // --- text panel ---
+        textPanel = new JPanel(new FlowLayout(FlowLayout.TRAILING));
+        textField = new JTextField();
+        textField.setFont(new Font(null,Font.PLAIN, 25));
+        textField.setText(txt);
+        textField.setEditable(false);
+        textPanel.add(textField);
+        textPanel.setBounds(0,0,200,50);
+
+        // --- add components to frame ---
+        this.add(numPanel);
+        this.add(funcPanel);
+        this.add(zePanel);
+        this.add(textPanel);
+
+        // --- launch frame ---
+        this.setSize(220,300);
+        this.setVisible(true);
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == numButtons[0]){
+            txt += "1";
+            return;
+        }
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    }
+
+}
+    /* 
     public String calculate(String input){
         try{
             int op = getOperator(input);
@@ -97,5 +181,11 @@ public class Calculator {
         }
         throw new InvalidInputException("Not a valid input");
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+    }
     
 }
+    */
